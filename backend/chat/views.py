ae0c32daf77f0ca443a335cb59268da34d570a3a
sobhan jafari <sobhanjafarii87@gmail.com>
2026-09-03 01:47:01 +0330

    feat: delete django backend

--- backend/chat/views.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import viewsets
-
-from .models import Message
-from .serializers import MessageSerializer
-
-class MessageViewSet(viewsets.ModelViewSet) :
-    queryset =  Message.objects.all()
-    serializer_class = MessageSerializer
