5788ae96f28bd868bdecf46cefeba7e91eb7c7b1
sobhan jafari <sobhanjafarii87@gmail.com>
2026-09-06 21:13:11 +0330

    [ADD] today history logic

--- frontend/src/components/History.tsx
+++ frontend/src/components/History.tsx
@@ -1,5 +1,16 @@
-import { ComponentProps, ReactElement } from "react";
+"use client";
+
+import {
+  ComponentProps,
+  Fragment,
+  ReactElement,
+  useEffect,
+  useState,
+} from "react";
 import Button from "./Button";
+import { getDate } from "@/data/date";
+import { FetchData } from "@/helpers/FetchData";
+import { MessageType } from "@/store/MessageStore";
 
 type Props = ComponentProps<"ul"> & {
   buttonClassName?: string;
@@ -12,6 +23,40 @@ export default function History({
   listClassName,
   ...otherProps
 }: Props): ReactElement {
+  const { year, month, day } = getDate();
+  const [toDay, setToDay] = useState<MessageType[]>([]);
+  const [lastWeek, setLastWeek] = useState<MessageType[]>([]);
+
+  useEffect(() => {
+    const handleChangeDate = async (): Promise<void> => {
+      const handleFetch = async (): Promise<MessageType[]> => {
+        const getHistory = await FetchData<MessageType[]>(
+          "http://localhost:8000/api/messages",
+        );
+
+        return getHistory;
+      };
+
+      const data = await handleFetch();
+      const freshDate: string = `${year}-${month}-${day}`;
+      const oldDate: string = `${year}-${month}-${parseInt(day) - 7}`;
+
+      const todayMessages: MessageType[] = data.filter(
+        (item) => item.date === freshDate,
+      );
+
+      setToDay(todayMessages);
+
+      const yesterdayMessages: MessageType[] = data.filter(
+        (item) => item.date === oldDate,
+      );
+
+      setLastWeek(yesterdayMessages);
+    };
+
+    handleChangeDate();
+  }, [year, month, day]);
+
   return (
     <div
       className={`w-full bg-white dark:bg-white/0 overflow-auto ${className}`}
@@ -26,37 +71,25 @@ export default function History({
         className={`w-full flex flex-col justify-start items-end text-right gap-3 pr-2 ${listClassName}`}
         {...otherProps}
       >
-        <h2 className="text-gray-600 dark:text-gray-500 text-lg">امروز</h2>
+        {toDay.length !== 0 && (
+          <h2 className="text-gray-600 dark:text-gray-500 text-lg">امروز</h2>
+        )}
 
-        <li className="w-full py-3 px-5 rounded-lg bg-gray-200 dark:bg-white/10">
-          پیشنویس ایمیل به تیم فروش
-        </li>
+        {toDay.map((chat) => {
+          const words = chat.content.split(" ");
+          const shortText =
+            words.length > 2 ? words.slice(0, 5).join(" ") + "..." : words;
 
-        <li className="w-full py-3 px-5 rounded-lg bg-gray-200 dark:bg-white/10">
-          خلاصه گذارش بازار سه ماهه
-        </li>
-
-        <h2 className="text-gray-600 dark:text-gray-500 text-lg">هفته گذشته</h2>
-
-        <li className="w-full py-3 px-5 rounded-lg bg-gray-200 dark:bg-white/10">
-          برنامه سفر به اصفهان
-        </li>
-
-        <li className="w-full py-3 px-5 rounded-lg bg-gray-200 dark:bg-white/10">
-          ویرایش رزومه کاری
-        </li>
-
-        <li className="w-full py-3 px-5 rounded-lg bg-gray-200 dark:bg-white/10">
-          ایده برای اسم فروشگاه
-        </li>
-
-        <li className="w-full py-3 px-5 rounded-lg bg-gray-200 dark:bg-white/10">
-          ایده برای اسم فروشگاه
-        </li>
-
-        <li className="w-full py-3 px-5 rounded-lg bg-gray-200 dark:bg-white/10">
-          ایده برای اسم فروشگاه
-        </li>
+          return (
+            <li
+              key={chat.id}
+              dir="rtl"
+              className="w-full py-3 px-5 rounded-lg bg-gray-200 dark:bg-white/10"
+            >
+              {shortText}
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
