e8ecf7a104946fdd8685292aac3076c8251b9187
sobhan jafari <sobhanjafarii87@gmail.com>
2026-09-07 19:56:54 +0330

    [FIX] manage routes

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,18 +1,21 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export const config = {
-  matcher: ["/chat", "/", "/auth/login"],
+  matcher: ["/chat/:path*", "/", "/auth/login"],
 };
 
 export function middleware(req: NextRequest) {
   const cookie = req.cookies.get("AUTH_TOKEN");
+
   const isAuthPage = req.nextUrl.pathname === "/auth/login";
+
   const isHomePage = req.nextUrl.pathname === "/";
 
   if (cookie) {
     if (isAuthPage || isHomePage) {
       return NextResponse.redirect(new URL("/chat", req.url));
     }
+
     return NextResponse.next();
   }
 
