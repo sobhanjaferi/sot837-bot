74bc4a98aa654ad8fd6d150179f800496ec47b13
sobhan jafari <sobhanjafarii87@gmail.com>
2026-09-07 14:50:11 +0330

    [PRETTIER] clear imports and class names

--- frontend/src/components/History.tsx
+++ frontend/src/components/History.tsx
@@ -1,12 +1,6 @@
 "use client";
 
-import {
-  ComponentProps,
-  Fragment,
-  ReactElement,
-  useEffect,
-  useState,
-} from "react";
+import { ComponentProps, ReactElement, useEffect, useState } from "react";
 import Button from "./Button";
 import { getDate } from "@/data/date";
 import { FetchData } from "@/helpers/FetchData";
@@ -23,9 +17,8 @@ export default function History({
   listClassName,
   ...otherProps
 }: Props): ReactElement {
-  const { year, month, day } = getDate();
+  const { year, month, day, hours, minutes } = getDate();
   const [toDay, setToDay] = useState<MessageType[]>([]);
-  const [lastWeek, setLastWeek] = useState<MessageType[]>([]);
 
   useEffect(() => {
     const handleChangeDate = async (): Promise<void> => {
@@ -39,19 +32,13 @@ export default function History({
 
       const data = await handleFetch();
       const freshDate: string = `${year}-${month}-${day}`;
-      const oldDate: string = `${year}-${month}-${parseInt(day) - 7}`;
+      const time: string = `${hours}:${minutes}`;
 
       const todayMessages: MessageType[] = data.filter(
         (item) => item.date === freshDate,
       );
 
       setToDay(todayMessages);
-
-      const yesterdayMessages: MessageType[] = data.filter(
-        (item) => item.date === oldDate,
-      );
-
-      setLastWeek(yesterdayMessages);
     };
 
     handleChangeDate();
@@ -59,32 +46,32 @@ export default function History({
 
   return (
     <div
-      className={`w-full bg-white dark:bg-white/0 overflow-auto ${className}`}
+      className={`w-full overflow-auto bg-white dark:bg-white/0 ${className}`}
     >
       <Button
-        className={`p-2 w-full border border-gray-400 dark:border-gray-500 text-cyan-700 dark:text-cyan-500 text-lg rounded-xl cursor-pointer transition-all duration-150 ease-in-out hover:-translate-y-1 active:opacity-30 mb-5 ${buttonClassName}`}
+        className={`mb-5 w-full cursor-pointer rounded-xl border border-gray-400 p-2 text-lg text-cyan-700 transition-all duration-150 ease-in-out hover:-translate-y-1 active:opacity-30 dark:border-gray-500 dark:text-cyan-500 ${buttonClassName}`}
       >
         گفتگو جدید +
       </Button>
 
       <ul
-        className={`w-full flex flex-col justify-start items-end text-right gap-3 pr-2 ${listClassName}`}
+        className={`flex w-full flex-col items-end justify-start gap-3 pr-2 text-right ${listClassName}`}
         {...otherProps}
       >
         {toDay.length !== 0 && (
-          <h2 className="text-gray-600 dark:text-gray-500 text-lg">امروز</h2>
+          <h2 className="text-lg text-gray-600 dark:text-gray-500">امروز</h2>
         )}
 
         {toDay.map((chat) => {
           const words = chat.content.split(" ");
           const shortText =
-            words.length > 2 ? words.slice(0, 5).join(" ") + "..." : words;
+            words.length > 2 ? words.slice(0, 5).join(" ") + " ..." : words;
 
           return (
             <li
               key={chat.id}
               dir="rtl"
-              className="w-full py-3 px-5 rounded-lg bg-gray-200 dark:bg-white/10"
+              className="w-full rounded-lg bg-gray-200 px-5 py-3 dark:bg-white/10"
             >
               {shortText}
             </li>
